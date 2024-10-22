b90720fbb7ee8da041c2f2a23187496dcfbaf85f
Nakulgoyal1303 <nakulgoyal298@gmail.com>
2024-10-22 13:03:12 +0530

    .

---
+++ Finance-App-master/Finance-App-master/lib/data/top.dart
@@ -0,0 +1,19 @@
+import 'package:managment/data/financial_data.dart';
+
+List<Money> getTopItems() {
+  Money snapFood = Money();
+  snapFood.time = 'jan 30,2022';
+  snapFood.image = 'mac.jpg';
+  snapFood.buy = true;
+  snapFood.fee = '- \$ 100';
+  snapFood.name = 'macdonald';
+
+  Money snap = Money();
+  snap.image = 'cre.png';
+  snap.time = 'today';
+  snap.buy = true;
+  snap.name = 'Transfer';
+  snap.fee = '- \$ 60';
+
+  return [snapFood, snap];
+}
---
+++ Finance-App-master/Finance-App-master/lib/widgets/bottomnavigationbar.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:managment/Screens/add.dart';
+import 'package:managment/Screens/home.dart';
+import 'package:managment/Screens/statistics.dart';
+
+class Bottom extends StatefulWidget {
+  const Bottom({Key? key}) : super(key: key);
+
+  @override
+  State<Bottom> createState() => _BottomState();
+}
+
+class _BottomState extends State<Bottom> {
+  int indexcolor = 0;
+  List screen = [
+    const Home(),
+    const Statistics(),
+    const Home(),
+    const Statistics()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screen[indexcolor],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => const AddScreen()));
+        },
+        backgroundColor: const Color(0xff368983),
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 7.5, bottom: 7.5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    indexcolor = 0;
+                  });
+                },
+                child: Icon(
+                  Icons.home,
+                  size: 30,
+                  color:
+                      indexcolor == 0 ? const Color(0xff368983) : Colors.grey,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    indexcolor = 1;
+                  });
+                },
+                child: Icon(
+                  Icons.bar_chart_outlined,
+                  size: 30,
+                  color:
+                      indexcolor == 1 ? const Color(0xff368983) : Colors.grey,
+                ),
+              ),
+              const SizedBox(width: 10),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    indexcolor = 2;
+                  });
+                },
+                child: Icon(
+                  Icons.account_balance_wallet_outlined,
+                  size: 30,
+                  color:
+                      indexcolor == 2 ? const Color(0xff368983) : Colors.grey,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    indexcolor = 3;
+                  });
+                },
+                child: Icon(
+                  Icons.person_outlined,
+                  size: 30,
+                  color:
+                      indexcolor == 3 ? const Color(0xff368983) : Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
